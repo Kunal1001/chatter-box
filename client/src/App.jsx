67409d080feb1fd30b6ad119073bb3a8bf3aad37
KUNAL KUMAR <96299735+Kunal1001@users.noreply.github.com>
2024-06-05 18:10:46 +0530

    Version 1.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,73 +1,10 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { io } from "socket.io-client";
 import axios from "axios";
-import {
-  Box,
-  Button,
-  Container,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
 import Room from "./components/Room";
 import SideNav from "./components/SideNav";
 import UserForm from "./components/UserForm";
-// const App = () => {
 
-
-
-
-//   return (
-//     <Container maxWidth="sm">
-//       <Box sx={{ height: 500 }} />
-//       <Typography variant="h6" component="div" gutterBottom>
-//         {socketID}
-//       </Typography>
-
-//       <form onSubmit={joinRoomHandler}>
-//         <h5>Join Room</h5>
-//         <TextField
-//           value={roomName}
-//           onChange={(e) => setRoomName(e.target.value)}
-//           id="outlined-basic"
-//           label="Room Name"
-//           variant="outlined"
-//         />
-//         <Button type="submit" variant="contained" color="primary">
-//           Join
-//         </Button>
-//       </form>
-
-//       <form onSubmit={handleSubmit}>
-//         <TextField
-//           value={message}
-//           onChange={(e) => setMessage(e.target.value)}
-//           id="outlined-basic"
-//           label="Message"
-//           variant="outlined"
-//         />
-//         <TextField
-//           value={room}
-//           onChange={(e) => setRoom(e.target.value)}
-//           id="outlined-basic"
-//           label="Room"
-//           variant="outlined"
-//         />
-//         <Button type="submit" variant="contained" color="primary">
-//           Send
-//         </Button>
-//       </form>
-
-//       <Stack>
-//         {messages.map((m, i) => (
-//           <Typography key={i} variant="h6" component="div" gutterBottom>
-//             {m}
-//           </Typography>
-//         ))}
-//       </Stack>
-//     </Container>
-//   );
-// };
 
 function App(){
 
